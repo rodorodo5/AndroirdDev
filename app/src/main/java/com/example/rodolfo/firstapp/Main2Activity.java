07c2b03e749b6db0452ebc50db65ea8d8eda7bec
Rodolfo Rodriguez <rodo@Rodolfos-MacBook-Pro.local>
2017-09-01 19:48:42 -0700

    :) Tarea 3

--- app/src/main/java/com/example/rodolfo/firstapp/Main2Activity.java
+++ app/src/main/java/com/example/rodolfo/firstapp/Main2Activity.java
@@ -3,8 +3,11 @@ package com.example.rodolfo.firstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import static com.example.rodolfo.firstapp.R.layout.activity_main2;
 
 
 public class Main2Activity extends AppCompatActivity {
@@ -12,13 +15,20 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(activity_main2);
 
         Intent intent = getIntent();
         String message = intent.getStringExtra("Mensaje");
-        TextView textView = (TextView) findViewById(R.id.textView1);
+        TextView txtShow = (TextView) findViewById(R.id.txtView);
+        Button btn_close = (Button) findViewById(R.id.btnBack);
+        txtShow.setText(message);
 
-
+        btn_close.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/rodolfo/firstapp/MainActivity.java
+++ app/src/main/java/com/example/rodolfo/firstapp/MainActivity.java
@@ -1,28 +1,40 @@
 package com.example.rodolfo.firstapp;
 
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.example.rodolfo.firstapp.models.Movie;
+import com.example.rodolfo.firstapp.models.MovieAdapter;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public ArrayList<Movie> movieList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Button btn_click = (Button) findViewById(R.id.btnClick);
+        Button btn_ListView = (Button) findViewById(R.id.listViewButton);
+        final EditText txt_name = (EditText) findViewById(R.id.txtName);
+
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,21 +43,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Toolbar toolbar1 = (Toolbar) findViewById(R.id.toolbar);
-        Button btn_click = (Button) findViewById(R.id.btnClick);
-        final EditText txtName = (EditText) findViewById(R.id.txtName);
-
-        btn_click.setOnClickListener(new View.OnClickListener(){
+        btn_click.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
-                EditText editText = (EditText) findViewById(R.id.txtName);
-                String message = txtName.getText().toString();
-                intent.putExtra("mensaje",message);
-                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_LONG).show();
+                Snackbar.make(view, txt_name.getText(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
 
+                Intent intentMain = new Intent(getApplicationContext(), Main2Activity.class);
+                EditText editText = (EditText) findViewById(R.id.txtName);
+                String message = editText.getText().toString();
+                intentMain.putExtra("Mensaje", message);
+                startActivity(intentMain);
 
             }
+        });
+
+        btn_ListView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                EditText editText = (EditText) findViewById(R.id.txtName);
+                String message = editText.getText().toString();
+
+
+                Movie oMovie = new Movie(
+                        message,
+                        "caca",
+                        "Rodo",
+                        "adsadsds",
+                        "05/08/54"
+                );
+
+                movieList.add(oMovie);
+                Intent intentMain = new Intent(getApplicationContext(), movieListActivity.class);
+                intentMain.putExtra("movieList", movieList);
+
+                startActivity(intentMain);
+
+            }
+
         });
 
     }
